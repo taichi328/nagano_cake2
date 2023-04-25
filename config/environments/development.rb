a4594807f9c9dba940b3c39f8a9c6d4ad3f8aaa7
EC2 Default User <125885895+taichi328@users.noreply.github.com>
2023-04-25 09:02:27 +0000

    [add]必要なコントローラの追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "79a7bbe60bc849b7b461b895161372c1.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
